6d34167975780ed58ecb822d95698ff00c88c871
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-11-26 20:19:03 +0600

    Signup complete, Integration test is remaining

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -6,5 +6,22 @@ class WorkersController < ApplicationController
   end
 
   def new
+    @worker = Worker.new
+  end
+
+  def create
+    @worker = Worker.new(worker_params)    # Not the final implementation!
+    if @worker.save
+      flash[:success] = "Welcome to the Work Abroad!"
+      redirect_to @worker
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def worker_params
+    params.require(:worker).permit(:fullname, :national_id, :email, :password, :password_confirmation)
   end
 end
--- app/helpers/workers_helper.rb
+++ app/helpers/workers_helper.rb
@@ -1,8 +1,9 @@
 module WorkersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for(worker)
+  def gravatar_for(worker, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(worker.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: worker.national_id, class: "gravatar")
   end
 end
